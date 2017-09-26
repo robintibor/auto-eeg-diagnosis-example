34666f573b3135f283063bdd41f8826492a27ef1
Robin Tibor Schirrmeister <robintibor@gmail.com>
2017-09-26 11:25:55 +0200

    config as paper

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 data_folders = [
     '/home/gemeinl/data/normal_abnormal/normalv1.1.1/v1.1.1/edf/',
     '/home/gemeinl/data/normal_abnormal/abnormalv1.1.1/v1.1.1/edf/']
-n_recordings = 500  # 3000 # max number, if you want to restrict set
+n_recordings = None  # set to an integer, if you want to restrict the set size
 sensor_types = ["EEG"]
 n_chans = 21
 max_recording_mins = 35  # exclude larger recordings from training set
@@ -13,16 +13,16 @@ sampling_freq = 100
 divisor = 10  # divide signal by this
 test_on_eval = True  # teston evaluation set or on training set
 # in case of test on eval, n_folds and i_testfold determine
-# validation fold in training set for early stop
+# validation fold in training set for training until first stop
 n_folds = 10
 i_test_fold = 9
 shuffle = True
 model_name = 'deep'
 n_start_chans = 25
-n_chan_factor = 2  # relevant for deep model
+n_chan_factor = 2  # relevant for deep model only
 input_time_length = 6000
 final_conv_length = 1
 model_constraint = 'defaultnorm'
 init_lr = 1e-3
 batch_size = 64
-max_epochs = 5  # 35
+max_epochs = 35 # until first stop, the continue train on train+valid
